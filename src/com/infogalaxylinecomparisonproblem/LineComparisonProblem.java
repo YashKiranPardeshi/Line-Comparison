2d965286b7a2c6026198032b0ecbd862701fd4ef
YashKiranPardeshi <yashpardeshi12300@gmail.com>
2022-03-11 10:56:23 +0530

    ADDED: Line Comparison problem for institute

---
+++ src/com/infogalaxylinecomparisonproblem/LineComparisonProblem.java
@@ -0,0 +1,8 @@
+package com.infogalaxylinecomparisonproblem;
+
+public class LineComparisonProblem {
+    public static void main(String[]args){
+        System.out.println("Welcome To Line Comparison Program Made by Yash");
+    }
+
+}
